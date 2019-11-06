bf7660b32e23a2bc787f5c18a4d44c5e370a79ea
Julien BASQUIN <jbasquin@iorga.com>
2019-11-06 18:44:09 +0100

    finition 3/4

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,19 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Doctrine\Common\Persistence\ObjectManager;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig', [
+            'nom' => 'julien',
+        ]);
+    }
+
+
     /**
      * @Route("/blog", name="blog")
      * @param ArticleRepository $repository
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
      */
     public function index(ArticleRepository $repository) // <--- ca c'est l'injection de dependence
     {
@@ -28,10 +44,11 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog/new", name="blog_create")
      * @Route("/blog/{id}/edit", name="blog_edit")
-     * @param Article $article
+     * @param Article|null $article
      * @param Request $request
      * @param ObjectManager $manager
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return Response
+     * @throws Exception
      */
     public function form(Article $article = null, Request $request, ObjectManager $manager)
     {
@@ -55,28 +72,42 @@ class BlogController extends AbstractController
         ]);
     }
 
+
     /**
      * @Route("/blog/{id}", name="blog_show", requirements={"id": "\d+"})
      * @param Article $article
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return Response
+     * @throws Exception
      * @internal param $id
      */
-    public function show(Article $article) // <--- ca c'est le param converter, il sait quel est l'article par rapport a l'id passé dans la route
+    public function show(Article $article = null, Request $request, ObjectManager $manager) // <--- ca c'est le param converter, il sait quel est l'article par rapport a l'id passé dans la route
     {
+        $comment = new Comment();
+
+        dump($comment);
+        $form = $this->createForm(CommentType::class, $comment);
+        dump($form);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            if ($article->getId() !== null) {
+                $comment->setCreatedAt(new \DateTime());
+            }
+//            $test = $comment->getArticle($article->getId());
+//            dump($test);
+            $manager->persist($comment);
+            $manager->flush();
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+        }
+
         return $this->render('blog/show.html.twig', [
             'article' => $article,
         ]);
     }
 
-    /**
-     * @Route("/", name="home")
-     */
-    public function home()
-    {
-        return $this->render('blog/home.html.twig', [
-            'nom' => 'julien',
-        ]);
-    }
+
 
 
 }
--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -3,22 +3,52 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class ArticlesFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 1; $i <=10; $i++){
-            $article = new Article();
-            $article->setTitle("Titre de l'article n°$i");
-            $article->setContent("Contenu de l'article n°$i");
-            $article->setImage("http://placehold.it/350x150");
-            $article->setCreatedAt(new \DateTime());
-            $manager->persist($article);
-        }
+        $faker = Factory::create('fr_FR');
 
+        // creer 3 category fake
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence());
+            $category->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            // creer entre 4 et 6 articles
+            for ($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $article = new Article();
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+                $article->setTitle($faker->sentence());
+                $article->setContent($content);
+                $article->setImage($faker->imageUrl());
+                $article->setCreatedAt($faker->dateTimeBetween('-6 months'));
+                $article->setCategory($category);
+
+                $manager->persist($article);
+
+                // on donne des commentaires a un article
+                for ($k = 1; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+                    $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+                    $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+                    $comment->setAuthor($faker->name);
+                    $comment->setContent($content);
+                    $comment->setCreatedAt($faker->dateTimeBetween('-' . $days . ' days'));
+                    $comment->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
+        }
         $manager->flush();
     }
 }
